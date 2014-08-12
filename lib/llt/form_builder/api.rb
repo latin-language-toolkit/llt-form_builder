56bd769013c5b41720643135da03eab4b0f804e2
LFDM <1986gh@gmail.com>
2014-08-12 19:20:00 +0200

    Add header controls to API

--- lib/llt/form_builder/api.rb
+++ lib/llt/form_builder/api.rb
@@ -7,6 +7,12 @@ require 'llt/form_builder'
 class Api < Sinatra::Base
   register Sinatra::RespondWith
 
+  before do
+    headers 'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => %w{ GET POST },
+      'Access-Control-Allow-Headers' => %w{ Content-Type }
+  end
+
   post '/generate' do
     request_json = JSON.parse(request.body.read)
     forms = LLT::FormBuilder.build(*request_json)
